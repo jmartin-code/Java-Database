cc14facffa9a78d2be884c5fe74b8cf2b43dc1da
Jonathan Martinez <68358371+jmartin-code@users.noreply.github.com>
2020-11-24 11:52:39 -0500

    Added .gitignore file to the project.

---
+++ src/jon/codes/Model/Album.java
@@ -0,0 +1,31 @@
+package jon.codes.Model;
+
+public class Album {
+    private int id;
+    private String name;
+    private String artistId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtistid() {
+        return artistId;
+    }
+
+    public void setArtistid(String artistid) {
+        this.artistId = artistid;
+    }
+}
